b71058470f7f15014da8ddfac917f7bdb04daa69
Shamsi <99732406+ajShamsi@users.noreply.github.com>
2022-09-30 22:11:23 +0600

    Add files via upload

---
+++ app/src/main/java/com/example/charity/UserProfile.java
@@ -0,0 +1,14 @@
+package com.example.charitynvet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
